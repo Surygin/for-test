87e7949d421ca79dfc8841e02562371078c0a53b
Anton Surygin <surygin@live.com>
2024-06-01 12:40:08 +0300

    Установли Breeze

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,81 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfileRequest;
-use App\Http\Resources\Profile\ProfileResource;
-use App\Models\Profile;
-use App\Models\User;
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display the user's profile form.
      */
-    public function index()
+    public function edit(Request $request): View
     {
-        $profiles = Profile::all();
-        $profiles = ProfileResource::collection($profiles)->resolve();
-
-        return $profiles;
+        return view('profile.edit', [
+            'user' => $request->user(),
+        ]);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Update the user's profile information.
      */
-//    public function create()
-//    {
-//        //
-//    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(ProfileRequest $request)
+    public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $data = ProfileResource::make($request)->resolve();
-        Profile::create($data);
+        $request->user()->fill($request->validated());
 
-        return http_response_code(200);
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
     /**
-     * Display the specified resource.
+     * Delete the user's account.
      */
-    public function show(Profile $profile)
+    public function destroy(Request $request): RedirectResponse
     {
-        dd($profile);
-        $profile = ProfileResource::make($profile)->resolve();
+        $request->validateWithBag('userDeletion', [
+            'password' => ['required', 'current_password'],
+        ]);
 
-        return $profile;
-    }
+        $user = $request->user();
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-//    public function edit(string $id)
-//    {
-//        //
-//    }
+        Auth::logout();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Profile $profile)
-    {
-        $data = ProfileResource::make($request)->resolve();
-        $profile->update($data);
+        $user->delete();
 
-        return 'Updated';
-    }
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Profile $profile)
-    {
-        $profile->delete();
-
-        return 'Destroy is done!';
+        return Redirect::to('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryControler;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\TagController;
-use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,55 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/testPost', [TestController::class, 'test']);
-Route::post('/testPost', [TestController::class, 'testPost']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Route::resource('posts', PostController::class);
-//
-////Route::prefix('posts')->group(function (){
-////    Route::get('/index',[PostController::class, 'index']);
-////    Route::get('/show/{post}',[PostController::class, 'show']);
-////    Route::get('/store',[PostController::class, 'store']);
-////    Route::get('/update/{post}',[PostController::class, 'update']);
-////    Route::get('/destroy/{post}',[PostController::class, 'destroy']);
-////});
-//
-//Route::prefix('categories')->group(function (){
-//    Route::get('/index',[CategoryControler::class, 'index']);
-//    Route::get('/show/{category}',[CategoryControler::class, 'show']);
-//    Route::get('/store',[CategoryControler::class, 'store']);
-//    Route::get('/update/{category}',[CategoryControler::class, 'update']);
-//    Route::get('/destroy/{category}',[CategoryControler::class, 'destroy']);
-//});
-//
-//Route::prefix('tags')->group(function (){
-//    Route::get('/index',[TagController::class, 'index']);
-//    Route::get('/show/{tag}',[TagController::class, 'show']);
-//    Route::get('/store',[TagController::class, 'store']);
-//    Route::get('/update/{tag}',[TagController::class, 'update']);
-//    Route::get('/destroy/{tag}',[TagController::class, 'destroy']);
-//});
-//
-//Route::prefix('comments')->group(function (){
-//    Route::get('/index',[CommentController::class, 'index']);
-//    Route::get('/show/{comment}',[CommentController::class, 'show']);
-//    Route::get('/store',[CommentController::class, 'store']);
-//    Route::get('/update/{comment}',[CommentController::class, 'update']);
-//    Route::get('/destroy/{comment}',[CommentController::class, 'destroy']);
-//});
-//
-//Route::prefix('roles')->group(function (){
-//    Route::get('/index',[RoleController::class, 'index']);
-//    Route::get('/show/{role}',[RoleController::class, 'show']);
-//    Route::get('/store',[RoleController::class, 'store']);
-//    Route::get('/update/{role}',[RoleController::class, 'update']);
-//    Route::get('/destroy/{role}',[RoleController::class, 'destroy']);
-//});
-//
-//Route::prefix('profiles')->group(function (){
-//    Route::get('/index',[ProfileController::class, 'index']);
-//    Route::get('/show/{profile}',[ProfileController::class, 'show']);
-//    Route::get('/store',[ProfileController::class, 'store']);
-//    Route::get('/update/{profile}',[ProfileController::class, 'update']);
-//    Route::get('/destroy/{profile}',[ProfileController::class, 'destroy']);
-//});
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
