72e220408241746b3ca29d491c608da60d315046
Anton Surygin <surygin@live.com>
2024-06-06 12:09:34 +0300

    переписал подключение middleware

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,14 @@ use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+Route::group([], function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class. 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
-});
+})->middleware([
+    'jwt.auth',
+    IsAdminMiddleware::class
+]);
 
 //Route::group(['middleware' => 'jwt.auth'],function (){
 //
